230589ab04f31684a76f69002981d72351508c2f
Khalid HAMDANI <khalidhamdani25@gmail.com>
2022-02-24 12:02:28 +0100

    changes

--- resources/js/pages/dashboard/connections/inc/overview.vue
+++ resources/js/pages/dashboard/connections/inc/overview.vue
@@ -180,7 +180,10 @@
 					<div class="flex group w-full">
 						<button @click="runConn(token)" type="button" class="p-3 pb-0 text-gray-400 hover:text-main-500 w-full">
 							<span class="flex flex-col items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-current text-gray-500 group-hover:text-gray-700 transition-color duration-200" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 24 24">
+                                <svg v-if="runningConn" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-current animate-spin text-gray-500 group-hover:text-gray-700 transition-color duration-200" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 24 24">
+                                    <path d="M13,3.233L13,3.233c0,0.454,0.302,0.867,0.745,0.966c3.711,0.829,6.46,4.238,6.243,8.242 c-0.221,4.086-3.662,7.437-7.752,7.555C7.718,20.127,4,16.49,4,12c0-3.807,2.675-6.996,6.243-7.798C10.69,4.101,11,3.693,11,3.235 V3.234c0-0.64-0.595-1.126-1.219-0.985C5.151,3.302,1.732,7.576,2.017,12.58c0.289,5.093,4.568,9.256,9.666,9.415 C17.341,22.171,22,17.619,22,12c0-4.753-3.334-8.741-7.785-9.752C13.593,2.107,13,2.595,13,3.233z"></path>
+                                </svg>
+                                <svg v-else xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-current text-gray-500 group-hover:text-gray-700 transition-color duration-200" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 24 24">
                                     <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 2c5.519 0 10 4.481 10 10s-4.481 10-10 10-10-4.481-10-10 4.481-10 10-10zm2 12v-3l5 4-5 4v-3h-9v-2h9zm-4-6v-3l-5 4 5 4v-3h9v-2h-9z"/>
                                 </svg>
 								<span class="my-2 transition-all duration-200">
@@ -254,6 +257,7 @@ export default {
             sheetFileOutput3: '',
             sheetOutput3: '',
             loading: true,
+            runningConn: false,
         }
     },
 
@@ -268,8 +272,10 @@ export default {
     },
 
     methods: {
-        runConn(token) {
-            this.$store.dispatch('run/updateConnection', {token: token});
+        async runConn(token) {
+            this.runningConn = true;
+            await this.$store.dispatch('run/updateConnection', {token: token});
+            this.runningConn = false;
             // Run.runConnection(token)
         },
         async loadConnection() {
